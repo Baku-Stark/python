3464be2451addc52a9d22fb6b5d14c252e5e5f5a
Baku Wall-e <103138773+Baku-Stark@users.noreply.github.com>
2022-05-31 09:56:52 -0300

    Update Curso Python #22 - Módulos e Pacotes.py

--- Curso Python #22 - Módulos e Pacotes.py
+++ Curso Python #22 - Módulos e Pacotes.py
@@ -1,15 +1,21 @@
 Aula 22 – Módulos e Pacotes
 
-Modularização --- Criar módulos
-
 def fatorial(n):
     f = 1
     for c in range(1, n +1):
         f *= c
     return f
-  
-  
+
+def dobro(n):
+    return n *2
+
+def triplo(n):
+    return n *3
+
+
 # ---
 num = int(input('Digite um valor: '))
 fat = fatorial(num)
 print('O fatorial de {}! é: {}.'.format(num, fat))
+print('>>> O dobro de {} é: {}.'.format(num, dobro(num))) 
+print('>>> O triplo de {} é: {}.'.format(num, triplo(num)))
