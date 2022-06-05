50b0a729af638ff67485bd2033517b2b54512637
Baku Wall-e <103138773+Baku-Stark@users.noreply.github.com>
2022-06-05 03:57:26 -0300

    LOGIN SYSTEM
    Função para login (receberá ajustes constantes)

---
+++ FRIDAY/Prática Friday - Login (Sublime Text).py
@@ -0,0 +1,23 @@
+#Login da armadura MD. Prime
+print('-=' *30)
+username = ''
+senhapass = ''
+user = input('>> Username: ')
+if user == 'WallaceIM':
+	#print('usuário confirmado!')
+	username = True
+else:
+	print('\033[1;31mUsuário não encontrado\033.')
+senha = input('>> Senha: ')
+if senha == 'wallace@123':
+	#print('senha confirmada!')
+	senhapass = True
+else:
+	print('\033[1;31mSenha incorreta.')
+print('')
+if username == True and senhapass == True:
+	print('--- LOGIN REALIZADO COM SUCESSO! BEM-VINDO {}! ---'.format(user))
+else:
+	print('--- VERIFIQUE SEU NOME DE USUÁRIO E/OU SENHA. ---')
+print('-=' *30)
+print('')
